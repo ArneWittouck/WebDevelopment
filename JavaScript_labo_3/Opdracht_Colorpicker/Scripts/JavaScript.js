9738eff717aced6e834389c467aa2ca647a2a6b3
ArneWittouck <r0928951@student.vives.be>
2023-03-08 21:51:03 +0100

    JS Labo 4, 1st commit

--- JavaScript_labo_3/Opdracht_Colorpicker/Scripts/JavaScript.js
+++ JavaScript_labo_3/Opdracht_Colorpicker/Scripts/JavaScript.js
@@ -3,12 +3,12 @@
 const setup = () => {
     let sliders = document.getElementsByClassName("slider");
 
-    sliders[0].addEventListener("change", update);
-    sliders[1].addEventListener("change", update);
-    sliders[2].addEventListener("change", update);
-    sliders[0].addEventListener("input", update);
-    sliders[1].addEventListener("input", update);
-    sliders[2].addEventListener("input", update);
+    for (let i = 0; i < sliders.length; i++) {
+        sliders[i].addEventListener("change", update);
+        sliders[i].addEventListener("input", update);
+    }
+
+    update();
 }
 
 const update = () => {
